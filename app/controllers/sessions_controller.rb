34d9155dc228fdd6362847e8c1ddde46cc8ff395
chausies <mahatmaajaytripathi@gmail.com>
2014-06-06 16:26:40 -0700

    Users index page working well with pagination

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		if !user
+			flash.now[:error] = 'This email is not registered'
+		elsif !user.authenticate(params[:session][:password])
+			flash.now[:error] = 'Invalid password'
+		else
 	    	sign_in user
-      		redirect_to user
-	  	else
-	    	flash.now[:error] = 'Invalid email/password combination' # Not quite right!
-      		render 'new'
+      		redirect_back_or user
 	  	end
 	end
 
